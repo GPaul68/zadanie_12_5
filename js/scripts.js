664b200afb27993bc2ec213baff8f5a5f072ba98
Paweł Galeja <pgaleja@gmail.com>
2018-01-21 20:58:40 +0100

    Add code in js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,37 @@
-function Button(text) {
-    this.text = text || 'Hello'
-};
+var prefix = "https://cors-anywhere.herokuapp.com/";
+var tweetLink = "https://twitter.com/intent/tweet?text=";
+var wuoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
 
-Button.prototype = {
-    create: function() {
-        var self = this;
-        this.$element = $('<button>');
-        this.$element.text(this.text);
-        this.$element.on('click', function() {
-            alert(self.text);
-        });
-        $('body').append(this.$element);
+function getQuote() {
+    $.getJSON(prefix + quoteUrl, createTweet);
+    $.ajaxSetup({ cache: false });
+}
+
+function createTweet(input) {
+    var data = input[0];
+    
+    var quoteText = $(data.content).text().trim();
+    var quoteAuthor = data.title;
+
+    if(!quoteAuthor.length) {
+        quoteAuthor = "Unknown author";
+    }
+
+    var tweetText = "Quote of the day - " + quoteText + "Author: " + quoteAuthor;
+
+    if (tweetText.length > 140) {
+        getQuote();
+    } else {
+        var tweet = tweetLink + encodeURIComponent(tweetText);
+        $('.quote').text(quoteText);
+        $('.author').text("Author: " + quoteAuthor);
+        $('.tweet').attr('href', tweet);
     }
 }
 
-var btn1 = new Button('Hello');
-btn1.create();
+$(document).ready(function() {
+    getQuote();
+    $('.trigger').on('click', function(){
+        getQuote();
+    });
+});
